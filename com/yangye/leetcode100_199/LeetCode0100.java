5b88171ed17e4f545fd8c19301b3b4a8e10919d3
yangye <956830514@qq.com>
2022-05-11 17:13:39 +0800

    first

---
+++ com/yangye/leetcode100_199/LeetCode0100.java
@@ -0,0 +1,8 @@
+package com.yangye.leetcode100_199;
+
+/**
+ * @Author: yangye
+ * @Date: 2022/5/11 17:11
+ */
+public class LeetCode0100 {
+}
---
+++ com/yangye/leetcode1_99/LeetCode0001.java
@@ -0,0 +1,8 @@
+package com.yangye.leetcode1_99;
+
+/**
+ * @Author: yangye
+ * @Date: 2022/5/11 17:10
+ */
+public class LeetCode0001 {
+}
