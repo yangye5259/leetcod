fe732d4c62a16395b02e3782fa821b7bf011f7bd
yangye <956830514@qq.com>
2022-05-13 22:50:27 +0800

    第六题

--- com/yangye/leetcode/leetcode1_99/LeetCode0012.java
+++ com/yangye/leetcode/leetcode1_99/LeetCode0012.java
@@ -134,4 +134,23 @@ public class LeetCode0012 {
         stringBuilder.append(strings[num]);
         return stringBuilder.toString();
     }
+
+    int[] values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+    String[] symbols = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+
+    public String intToRoman1(int num) {
+        StringBuilder roman = new StringBuilder();
+        for (int i = 0; i < values.length; ++i) {
+            int value = values[i];
+            String symbol = symbols[i];
+            while (num >= value) {
+                num -= value;
+                roman.append(symbol);
+            }
+            if (num == 0) {
+                break;
+            }
+        }
+        return roman.toString();
+    }
 }
