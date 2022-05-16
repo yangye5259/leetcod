6312b000d6e625b5271ded2f404d16f223c74a13
yangye <956830514@qq.com>
2022-05-16 10:02:59 +0800

    刷题！

--- com/yangye/leetcode/leetcode1_99/LeetCode0022.java
+++ com/yangye/leetcode/leetcode1_99/LeetCode0022.java
@@ -1,6 +1,6 @@
 package com.yangye.leetcode.leetcode1_99;
 
-import java.util.LinkedList;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -44,31 +44,29 @@ public class LeetCode0022 {
     }
 
     public List<String> generateParenthesis(int n) {
-        LinkedList<LinkedList<String>> result = new LinkedList<>();
-        if (n == 0)
-            return result.get(0);
-        LinkedList<String> list0 = new LinkedList<>();
-        list0.add("");
-        result.add(list0);
-        LinkedList<String> list1 = new LinkedList<>();
-        list1.add("()");
-        result.add(list1);
-        for (int i = 2; i <= n; i++) {
-            LinkedList<String> temp = new LinkedList<>();
-            for (int j = 0; j < i; j++) {
-                List<String> str1 = result.get(j);
-                List<String> str2 = result.get(i - 1 - j);
-                for (String s1 : str1) {
-                    for (String s2 : str2) {
-                        String el = "(" + s1 + ")" + s2;
-                        temp.add(el);
+        List<String> result = new ArrayList<>();
+        if (n == 0) {
+            result.add("");
+            return result;
+        }
+        if (n == 1) {
+            result.add("()");
+            return result;
+        }
+
+        for (int i = 0; i < n; i++) {
+            List<String> list1 = generateParenthesis(i);
+            List<String> list2 = generateParenthesis(n - 1 - i);
+            for (String s1 : list1) {
+                for (String s2 : list2) {
+                    String temp = "(" + s1 + ")" + s2;
+                    if (!result.contains(temp)) {
+                        result.add(temp);
                     }
                 }
-
             }
-            result.add(temp);
         }
-        return result.get(n);
+        return result;
     }
 
 
